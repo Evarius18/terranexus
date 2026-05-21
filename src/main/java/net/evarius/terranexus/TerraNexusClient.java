ffea8225ba175fec34d75d6653112795beedc09a
Evarius18 <fabiangaming.de20@gmail.com>
2026-05-21 11:55:15 +0200

    Initial commit

---
+++ src/main/java/net/evarius/terranexus/TerraNexusClient.java
@@ -0,0 +1,10 @@
+package net.evarius.terranexus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerraNexusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
