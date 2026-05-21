a70da6de027c87b9db8046c432c0cf3e36479657
Evarius18 <fabiangaming.de20@gmail.com>
2026-05-21 18:33:32 +0200

    Custom Recipes & Loot Tables - there is still a issue with mining these ores

--- src/main/java/net/evarius/terranexus/block/ModBlocks.java
+++ src/main/java/net/evarius/terranexus/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.evarius.terranexus.TerraNexus;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,19 +14,34 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+
+import java.util.function.Function;
 
 public class ModBlocks {
+
+    // Pink Garnet Blocks
     public static Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
             AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK));
-
     public static Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK));
 
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            AbstractBlock.Settings.create().strength(3f).requiresTool(),
+            (settings) -> new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), settings));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE),
+            (settings) -> new ExperienceDroppingBlock(UniformIntProvider.create(3, 6), settings));
+
     private static Block registerBlock(String name, AbstractBlock.Settings settings) {
+        return registerBlock(name, settings, Block::new);
+    }
+
+    private static Block registerBlock(String name, AbstractBlock.Settings settings, Function<AbstractBlock.Settings, Block> blockFactory) {
         RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TerraNexus.MOD_ID, name));
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TerraNexus.MOD_ID, name));
 
-        Block block = new Block(settings.registryKey(blockKey));
+        Block block = blockFactory.apply(settings.registryKey(blockKey));
 
         Registry.register(Registries.ITEM, itemKey,
                 new BlockItem(block, new Item.Settings().registryKey(itemKey)));
@@ -39,6 +55,8 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
